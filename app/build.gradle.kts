ec608f17e5855ec8b8aeb18a9fe8297753b67ad8
karim.2501 <karimmitra0@gmail.com>
2023-11-29 22:53:15 +0700

    Fitur chat dan login sudah jalan, namun harus ada perbaikan

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,10 +48,10 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.recyclerview:recyclerview:1.3.0")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.5")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.5")
-    implementation("androidx.preference:preference:1.2.1")
+//    implementation("androidx.preference:preference-ktx:1.2.1")
     implementation("androidx.annotation:annotation:1.7.0")
 
     //Testing
@@ -73,8 +73,10 @@ dependencies {
     ksp("androidx.room:room-compiler:2.6.0")
 
     //Firebase
-    implementation("com.google.firebase:firebase-auth-ktx:22.3.0")
     implementation("com.google.android.gms:play-services-auth:20.7.0")
+    implementation("com.google.firebase:firebase-auth-ktx:22.3.0")
+    implementation("com.google.firebase:firebase-database-ktx:20.3.0")
+    implementation("com.firebaseui:firebase-ui-database:8.0.0")
 
     //Coroutine
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
--- app/src/main/java/com/mhmdnurulkarim/hukumq/ui/home/HomeFragment.kt
+++ app/src/main/java/com/mhmdnurulkarim/hukumq/ui/home/HomeFragment.kt
@@ -4,39 +4,99 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.firebase.ui.database.FirebaseRecyclerOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import com.mhmdnurulkarim.hukumq.R
+import com.mhmdnurulkarim.hukumq.data.model.Message
 import com.mhmdnurulkarim.hukumq.databinding.FragmentHomeBinding
+import com.mhmdnurulkarim.hukumq.ui.adapter.MessageAdapter
+import java.util.Date
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+    private lateinit var auth: FirebaseAuth
+    private lateinit var db: FirebaseDatabase
+    private lateinit var adapter: MessageAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
-
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        return binding.root
+    }
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        auth = Firebase.auth
+        val firebaseUser = auth.currentUser
+
+        db = Firebase.database
+        val messagesRef = db.reference.child(MESSAGES_CHILD)
+
+        binding.sendButton.setOnClickListener {
+            val friendlyMessage = Message(
+                binding.messageEditText.text.toString(),
+                firebaseUser?.displayName.toString(),
+                firebaseUser?.photoUrl.toString(),
+                Date().time
+            )
+            messagesRef.push().setValue(friendlyMessage) { error, _ ->
+                if (error != null) {
+                    Toast.makeText(
+                        requireActivity(),
+                        getString(R.string.send_error) + error.message,
+                        Toast.LENGTH_SHORT
+                    ).show()
+                } else {
+                    Toast.makeText(
+                        requireActivity(),
+                        getString(R.string.send_success),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+            binding.messageEditText.setText("")
         }
-        return root
+
+        val manager = LinearLayoutManager(requireActivity())
+        manager.stackFromEnd = true
+        binding.messageRecyclerView.layoutManager = manager
+
+        val options = FirebaseRecyclerOptions.Builder<Message>()
+            .setQuery(messagesRef, Message::class.java)
+            .build()
+        adapter = MessageAdapter(options, firebaseUser?.displayName)
+        binding.messageRecyclerView.adapter = adapter
+    }
+
+    override fun onResume() {
+        super.onResume()
+        adapter.startListening()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        adapter.stopListening()
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    companion object {
+        const val MESSAGES_CHILD = "messages"
     }
 }
